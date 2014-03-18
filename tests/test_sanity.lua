0ab139e3ad935b3d86fa50c44e2da18b15f2c673
Jiale Zhi <vipcalio@gmail.com>
2014-03-18 14:52:09 -0700

    Remove an obsolete test case

--- tests/test_sanity.lua
+++ tests/test_sanity.lua
@@ -25,19 +25,12 @@ end
 function test_new()
    local rvn, msg = raven:new("https://public:secret@example.com/sentry/project-id")
    assert_not_nil(rvn)
-   assert_equal("Lua Sentry Client/0.4", rvn.client_id)
+   assert_equal("raven-lua/0.4", rvn.client_id)
 
    -- missing public key in DSN
    local rvn1, msg = raven:new("https://secret@example.com/sentry/project-id")
    assert_nil(rvn1)
    assert_equal("Bad DSN", msg)
-end
-
-function test_get_debug_info()
-   local culprit, stack = raven.get_debug_info()
-   --assert_equal("tests/test_sanity.lua:33", culprit)
-   assert_not_nil(string_match(culprit, "tests/test_sanity.lua:%d+"))
-   assert_not_nil(stack)
 end
 
 function test_parse_host_port()
