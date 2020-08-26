ada30438ffcafe1249477c7318bd70836d1ce891
Sergey Nevmerzhitsky <twinchic@gmail.com>
2020-08-27 01:18:08 +0400

    Add tests for the new feature

--- tests/util.lua
+++ tests/util.lua
@@ -41,6 +41,19 @@ describe("util functions", function()
         assert_equal(out.long_host, "sentry.example.com:1234")
         assert_equal(out.request_uri, "/api/1/store/")
         assert_equal(out.server, "http://sentry.example.com:1234/api/1/store/")
+
+        -- new auth-style DSN
+        local out = util.parse_dsn("http://public@sentry.example.com/1")
+        assert_equal(out.protocol, "http")
+        assert_equal(out.public_key, "public")
+        assert_equal(out.secret_key, nil)
+        assert_equal(out.host, "sentry.example.com")
+        assert_equal(out.port, 80)
+        assert_equal(out.project_id, "1")
+        -- aux fields
+        assert_equal(out.long_host, "sentry.example.com")
+        assert_equal(out.request_uri, "/api/1/store/")
+        assert_equal(out.server, "http://sentry.example.com:80/api/1/store/")
     end)
 
     test("Auth header generation", function()
@@ -55,6 +68,21 @@ describe("util functions", function()
         assert_equal(params.sentry_version, "6")
         assert_equal(params.sentry_key, "public")
         assert_equal(params.sentry_secret, "secret")
+        assert_type(params.sentry_client, "string")
+        assert_type(params.sentry_timestamp, "string")
+
+        -- new auth-style DSN
+        local dsn = util.parse_dsn("https://public@sentry.example.com/1")
+        local auth = util.generate_auth_header(dsn)
+        assert_equal(auth:match("Sentry .+"), auth)
+        local params = {}
+        for k, v in auth:gmatch("(%S-)=([^,]+)") do
+            params[k] = v
+        end
+
+        assert_equal(params.sentry_version, "6")
+        assert_equal(params.sentry_key, "public")
+        assert_equal(params.sentry_secret, nil)
         assert_type(params.sentry_client, "string")
         assert_type(params.sentry_timestamp, "string")
     end)
